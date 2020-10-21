351f1fbb14bf4cb82a7fbcace1e44f9e7a5f45b2
Gal Halali <galh@wix.com>
2020-10-21 15:30:14 +0300

    all tests implemented

--- src/game.js
+++ src/game.js
@@ -1,5 +1,52 @@
 class Game {
+    constructor() {
+    }
 
+    initializeBoard(board) {
+        this.board = this.createBoardCopy(board);
+    }
+
+    getBoard() {
+        return this.board;
+    }
+
+    createBoardCopy(board) {
+        return board.map(arr => arr.slice());
+    }
+
+    cellInBound(row, col) {
+        return row >= 0 && row < this.board.length && col >= 0 && col < this.board[row].length;
+    }
+
+    countNeighbors(row, col) {
+        let aliveNeighbors = 0;
+
+        for (let rowOffset = -1; rowOffset <= 1; rowOffset++) {
+            for (let colOffset = -1; colOffset <= 1; colOffset++) {
+                if (this.cellInBound(row + rowOffset, col + colOffset)) {
+                    aliveNeighbors += this.board[row + rowOffset][col + colOffset];
+                }
+            }
+        }
+        
+        aliveNeighbors -= this.board[row][col];
+
+        return aliveNeighbors;
+    }
+
+    step() {
+        const boardCpy = this.createBoardCopy(this.board);
+
+        for (let row = 0; row < this.board.length; row++) {
+            for (let col = 0; col < this.board[row].length; col++) {
+                const aliveNeighbors = this.countNeighbors(row, col);
+                
+                boardCpy[row][col] = (aliveNeighbors >= 2 && aliveNeighbors <= 3) ? 1 : 0;
+            }
+        }
+
+        this.board = boardCpy;
+    }
 }
 
 module.exports = Game;
--- test/game.spec.js
+++ test/game.spec.js
@@ -1,7 +1,68 @@
+const { expect } = require("chai");
 const Game = require("../src/game");
 
 describe("Game test", () => {
-    it("Initialize game", () => {
-        const game = new Game();
+    let game;
+
+    beforeEach(() => {
+        game = new Game();
+    });
+
+    function createBoard(rows, cols, init) {
+        const board = Array(rows).fill().map(() => Array(cols).fill(init));
+
+        return board;
+    }
+
+    it("Initialize game with 2x2 empty board", () => {
+        game.initializeBoard(createBoard(2, 2, 0));
+
+        expect(game.getBoard()).to.eql(createBoard(2, 2, 0));
+    });
+
+    it("game with 1 cell should die", () => {
+        const board = createBoard(2, 2);
+        board[0][0] = 1;
+        game.initializeBoard(board);
+        game.step();
+
+        expect(game.getBoard()).to.eql(createBoard(2, 2, 0));
+    });
+
+    it("game with 2 cells should die", () => {
+        const board = createBoard(4, 4);
+        board[0][0] = 1;
+        board[3][3] = 1;
+        game.initializeBoard(board);
+        game.step();
+
+        expect(game.getBoard()).to.eql(createBoard(4, 4, 0));
+    });
+
+    it("game with 3 cells all should live", () => {
+        const board = createBoard(2, 2, 0);
+        board[0][0] = 1;
+        board[0][1] = 1;
+        board[1][0] = 1;
+
+        game.initializeBoard(board);
+        game.step();
+
+        expect(game.getBoard()).to.eql(createBoard(2, 2, 1));
+    });
+
+    it("game with 2 cells 2 should live, 2 should die", () => {
+        const board = createBoard(2, 2, 0);
+        board[0][0] = 1;
+        board[0][1] = 1;
+
+        game.initializeBoard(board);
+        game.step();
+
+        const resultBoard = createBoard(2, 2, 0);
+        resultBoard[1][0] = 1;
+        resultBoard[1][1] = 1;
+
+        expect(game.getBoard()).to.eql(resultBoard);
     });
 });
