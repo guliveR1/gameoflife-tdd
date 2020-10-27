27586a0d9fd1b8f4d1e1dd05e8029321ca919464
Gal Halali <galh@wix.com>
2020-10-27 14:55:23 +0200

    Refactored tests

--- test/game.spec.js
+++ test/game.spec.js
@@ -8,61 +8,71 @@ describe("Game test", () => {
         game = new Game();
     });
 
-    function createBoard(rows, cols, init) {
-        const board = Array(rows).fill().map(() => Array(cols).fill(init));
-
-        return board;
-    }
-
     it("Initialize game with 2x2 empty board", () => {
-        game.initializeBoard(createBoard(2, 2, 0));
+        game.initializeBoard([
+            [0, 0],
+            [0, 0]
+        ]);
 
-        expect(game.getBoard()).to.eql(createBoard(2, 2, 0));
+        expect(game.getBoard()).to.eql([
+            [0, 0],
+            [0, 0]
+        ]);
     });
 
-    it("game with 1 cell should die", () => {
-        const board = createBoard(2, 2);
-        board[0][0] = 1;
-        game.initializeBoard(board);
+    it("1 cell with no neighbours should die", () => {
+        game.initializeBoard([
+            [1, 0],
+            [0, 0]
+        ]);
         game.step();
 
-        expect(game.getBoard()).to.eql(createBoard(2, 2, 0));
+        expect(game.getBoard()).to.eql([
+            [0, 0],
+            [0, 0]
+        ]);
     });
 
-    it("game with 2 cells should die", () => {
-        const board = createBoard(4, 4);
-        board[0][0] = 1;
-        board[3][3] = 1;
-        game.initializeBoard(board);
+    it("2 cells with no neighbours should die", () => {
+        game.initializeBoard([
+            [1, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 1]
+        ]);
         game.step();
 
-        expect(game.getBoard()).to.eql(createBoard(4, 4, 0));
+        expect(game.getBoard()).to.eql([
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0 ,0]
+        ]);
     });
 
-    it("game with 3 cells all should live", () => {
-        const board = createBoard(2, 2, 0);
-        board[0][0] = 1;
-        board[0][1] = 1;
-        board[1][0] = 1;
-
-        game.initializeBoard(board);
+    it("when there are 3 cells alive, all cells should be alive", () => {
+        game.initializeBoard([
+            [1, 1],
+            [1, 0]
+        ]);
         game.step();
 
-        expect(game.getBoard()).to.eql(createBoard(2, 2, 1));
+        expect(game.getBoard()).to.eql([
+            [1, 1],
+            [1, 1]
+        ]);
     });
 
-    it("game with 2 cells 2 should live, 2 should die", () => {
-        const board = createBoard(2, 2, 0);
-        board[0][0] = 1;
-        board[0][1] = 1;
-
-        game.initializeBoard(board);
+    it("when there are 2 cells alive they should die and their neighbours should live", () => {
+        game.initializeBoard([
+            [1, 1],
+            [0, 0]
+        ]);
         game.step();
 
-        const resultBoard = createBoard(2, 2, 0);
-        resultBoard[1][0] = 1;
-        resultBoard[1][1] = 1;
-
-        expect(game.getBoard()).to.eql(resultBoard);
+        expect(game.getBoard()).to.eql([
+            [0, 0],
+            [1, 1]
+        ]);
     });
 });
